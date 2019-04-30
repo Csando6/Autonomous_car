4c3f2d81ea0364e78b24818b4c525958fbeb504d
csandoval <csando6@uic.edu>
2019-04-30 18:15:34 -0500

    added serial read

--- car_led/car_led/car_led.ino
+++ car_led/car_led/car_led.ino
@@ -1,3 +1,5 @@
+char inputChar;
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -5,5 +7,17 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if(Serial.available()>0){//reads input from the tx device
+    while(Serial.available() > 0)
+      inputChar = char(Serial.read());
+    Serial.write(inputChar);//sends value to kaylyns arduino
+  }
+
+  switch(inputChar){
+    case 'O':
+      break;
+    case 'F':
+      break;
+  }
 
 }
